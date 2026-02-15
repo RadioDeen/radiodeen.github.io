7708f49c7505928633f59230fc65ea69646e7ee5
RadioDeen <91003421+RadioDeen@users.noreply.github.com>
2026-02-15 16:37:23 +0600

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -4,37 +4,100 @@ import { Gojol } from './types';
 export const GOJOL_LIST: Gojol[] = [
   {
     id: '1',
-    title: 'আল্লাহর রহমত',
+    title: 'আমি চিৎকার করে কাঁদিতে চাহিয়া',
     artist: 'মাহমুদুল হাসান',
-    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Ami_chitkar_kore_kadite_chahiya.mp3',
     thumbnail: 'https://picsum.photos/seed/islam1/400/400'
   },
   {
     id: '2',
-    title: 'নবীজির শানে',
+    title: 'দাও খোদা দাও',
     artist: 'আবু রায়হান',
-    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Dao_Khoda_dao_amay_abar.mp3',
     thumbnail: 'https://picsum.photos/seed/islam2/400/400'
   },
   {
     id: '3',
-    title: 'রমজানের গান',
+    title: 'এতো ভালোবাসো কেন মালিক আমায়',
     artist: 'কলরব',
-    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Eto_valobaso_keno_Malik.mp3',
     thumbnail: 'https://picsum.photos/seed/islam3/400/400'
   },
   {
     id: '4',
-    title: 'জান্নাতের সুসংবাদ',
+    title: 'হারদাম আল্লাহ আল্লাহ কার',
     artist: 'সাঈদ আহমদ',
-    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Hardam_Allah.mp3',
     thumbnail: 'https://picsum.photos/seed/islam4/400/400'
   },
   {
     id: '5',
-    title: 'মৃত্যুর ডাক',
+    title: 'হে রাসূল বুঝিনা আমি',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/He_rasul_bujhina_ami.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '6',
+    title: 'হৃদয় মাঝে মালা গাঁথি',
+    artist: 'তরিকুল ইসলাম',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Hridoy_majhe_mala_gathi.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '7',
+    title: 'যদি নাত লিখতে লিখতে',
+    artist: 'তরিকুল ইসলাম',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Jodi_naat_likhte_likhte.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '8',
+    title: 'কত দূর ঐ মদীনার পথ',
+    artist: 'তরিকুল ইসলাম',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Koto_dur_oi_madinar_poth.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '9',
+    title: 'লুকোনো ফুল',
+    artist: 'তরিকুল ইসলাম',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Lukono_ful.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '10',
+    title: 'মে বান্দায়ে আছি হুঁ',
+    artist: 'তরিকুল ইসলাম',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Main_banda_e_aasi_hoon.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '11',
+    title: 'মুজে কুফা ওয়ালো',
+    artist: 'তরিকুল ইসলাম',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Mujhe_kufawalo.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '12',
+    title: 'নাতে সারকার',
+    artist: 'তরিকুল ইসলাম',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Naate_sarkar_ki_parhta_hoon.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '13',
+    title: 'ওগো রব',
+    artist: 'তরিকুল ইসলাম',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Ogo_Rab.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '14',
+    title: 'হে মেরা নাবী হে',
+    artist: 'তরিকুল ইসলাম',
+    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Woh_mera_nabi_hai.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   }
 ];
