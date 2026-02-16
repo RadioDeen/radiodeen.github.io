365fa90d611dbf398fff350a3eec1eb8bd1120cb
RadioDeen <91003421+RadioDeen@users.noreply.github.com>
2026-02-16 14:04:13 +0600

    Change audio URLs to Cloudinary links
    Updated audio URLs for multiple songs to use Cloudinary links.

--- constants.ts
+++ constants.ts
@@ -6,98 +6,98 @@ export const GOJOL_LIST: Gojol[] = [
     id: '1',
     title: 'আমি চিৎকার করে কাঁদিতে চাহিয়া',
     artist: 'মাহমুদুল হাসান',
-    url: 'https://cdn.pixabay.com/download/audio/2026/02/15/audio_9fabd34df8.mp3?filename=u_3qc2kfuonp-ami_chitkar_kore_kadite_chahiya-485249.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228461/Ami_chitkar_kore_kadite_chahiya_p7wq6j.mp3',
     thumbnail: 'https://picsum.photos/seed/islam1/400/400'
   },
   {
     id: '2',
     title: 'দাও খোদা দাও',
     artist: 'আবু রায়হান',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Dao_Khoda_dao_amay_abar.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228466/Dao_Khoda_dao_amay_abar_b2scd7.mp3',
     thumbnail: 'https://picsum.photos/seed/islam2/400/400'
   },
   {
     id: '3',
     title: 'এতো ভালোবাসো কেন মালিক আমায়',
     artist: 'কলরব',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Eto_valobaso_keno_Malik.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228476/Eto_valobaso_keno_Malik_drxkg2.mp3',
     thumbnail: 'https://picsum.photos/seed/islam3/400/400'
   },
   {
     id: '4',
     title: 'হারদাম আল্লাহ আল্লাহ কার',
     artist: 'সাঈদ আহমদ',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Hardam_Allah.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228463/Hardam_Allah_u9r3qt.mp3',
     thumbnail: 'https://picsum.photos/seed/islam4/400/400'
   },
   {
     id: '5',
     title: 'হে রাসূল বুঝিনা আমি',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/He_rasul_bujhina_ami.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228500/He_rasul_bujhina_ami_ziqe8v.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '6',
     title: 'হৃদয় মাঝে মালা গাঁথি',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Hridoy_majhe_mala_gathi.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228470/Hridoy_majhe_mala_gathi_kdfhk9.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '7',
     title: 'যদি নাত লিখতে লিখতে',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Jodi_naat_likhte_likhte.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228498/Jodi_naat_likhte_likhte_ktnbcv.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '8',
     title: 'কত দূর ঐ মদীনার পথ',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Koto_dur_oi_madinar_poth.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228508/Koto_dur_oi_madinar_poth_yqey6f.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '9',
     title: 'লুকোনো ফুল',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Lukono_ful.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228521/Lukono_ful_cfwyg9.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '10',
     title: 'মে বান্দায়ে আছি হুঁ',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Main_banda_e_aasi_hoon.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228528/Main_banda_e_aasi_hoon_yggyox.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '11',
     title: 'মুজে কুফা ওয়ালো',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Mujhe_kufawalo.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228502/Mujhe_kufawalo_bwhdws.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '12',
     title: 'নাতে সারকার',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Naate_sarkar_ki_parhta_hoon.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228519/Naate_sarkar_ki_parhta_hoon_n032mq.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '13',
     title: 'ওগো রব',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Ogo_Rab.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228534/Ogo_Rab_rzamrv.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '14',
     title: 'হে মেরা নাবী হে',
     artist: 'তরিকুল ইসলাম',
-    url: 'https://rawcdn.githack.com/RadioDeen/radiodeen.github.io/9f9ed60721aa0840db9b54d0752af2a434b13e1b/Islamic_Song/Woh_mera_nabi_hai.mp3',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228537/Woh_mera_nabi_hai_x5otd7.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   }
 ];
