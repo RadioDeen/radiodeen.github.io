8b039fd7616321ec6befc203727dfa9aa1854bc4
RadioDeen <91003421+RadioDeen@users.noreply.github.com>
2026-02-17 14:04:11 +0600

    Update GOJOL_LIST and QURAN_SCHEDULE titles and URLs

--- constants.ts
+++ constants.ts
@@ -4,60 +4,123 @@ import { Gojol, ScheduledProgram } from './types';
 export const GOJOL_LIST: Gojol[] = [
   {
     id: '1',
-    title: 'আমি চিৎকার করে কাঁদিতে চাহিয়া',
-    artist: 'মাহমুদুল হাসান',
-    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228461/Ami_chitkar_kore_kadite_chahiya_p7wq6j.mp3',
+    title: 'ওগো রব',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312531/Ogo_Rab_kwuhd4.mp3',
     thumbnail: 'https://picsum.photos/seed/islam1/400/400'
   },
   {
     id: '2',
-    title: 'দাও খোদা দাও',
-    artist: 'আবু রায়হান',
-    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228466/Dao_Khoda_dao_amay_abar_b2scd7.mp3',
+    title: 'মেঁ বান্দাহে আছি হুঁ',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312531/Main_banda_e_aasi_hoon_mwdj7n.mp3',
     thumbnail: 'https://picsum.photos/seed/islam2/400/400'
   },
   {
     id: '3',
-    title: 'এতো ভালোবাসো কেন মালিক আমায়',
-    artist: 'কলরব',
-    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228476/Eto_valobaso_keno_Malik_drxkg2.mp3',
+    title: 'লুকোনো ফুল',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312528/Lukono_ful_w7smkp.mp3',
     thumbnail: 'https://picsum.photos/seed/islam3/400/400'
   },
   {
     id: '4',
-    title: 'হারদাম আল্লাহ আল্লাহ কার',
-    artist: 'সাঈদ আহমদ',
-    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228463/Hardam_Allah_u9r3qt.mp3',
+    title: 'মেরা নাবী হেঁ',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312525/Woh_mera_nabi_hai_zvaxlm.mp3',
     thumbnail: 'https://picsum.photos/seed/islam4/400/400'
   },
   {
     id: '5',
-    title: 'হে রাসূল বুঝিনা আমি',
-    artist: 'তরিকুল ইসলাম',
-    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228500/He_rasul_bujhina_ami_ziqe8v.mp3',
+    title: 'মুজে কুফা ওয়ালো',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312519/Mujhe_kufawalo_jfzbxb.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   },
   {
     id: '6',
+    title: 'নাতে সারকার',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312513/Naate_sarkar_ki_parhta_hoon_evrfj0.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '7',
+    title: 'কতদূর ঐ মদীনার পথ',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312503/Koto_dur_oi_madinar_poth_fysg8u.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '8',
+    title: 'যদি নাত লিখতে লিখতে',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312495/Jodi_naat_likhte_likhte_uo3iqd.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '9',
+    title: 'দাও খোদা দাও আমায় আবার',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312476/Dao_Khoda_dao_amay_abar_bnlyzh.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '10',
     title: 'হৃদয় মাঝে মালা গাঁথি',
-    artist: 'তরিকুল ইসলাম',
-    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771228470/Hridoy_majhe_mala_gathi_kdfhk9.mp3',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312475/Hridoy_majhe_mala_gathi_si1dgs.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '11',
+    title: 'আমি চিৎকার করে কাঁদিতে চাহিয়া',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312473/Ami_chitkar_kore_kadite_chahiya_noalsh.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '12',
+    title: 'এতো ভালোবাসো কেন মালিক আমায়',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312473/Eto_valobaso_keno_Malik_i4gte8.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '13',
+    title: 'হে রাসূল বুঝিনা আমি',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312469/He_rasul_bujhina_ami_zfnlkn.mp3',
+    thumbnail: 'https://picsum.photos/seed/islam5/400/400'
+  },
+  {
+    id: '14',
+    title: 'হারদাম আল্লাহ আল্লাহ কার',
+    artist: ' ',
+    url: 'https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312456/Hardam_Allah_uihcy6.mp3',
     thumbnail: 'https://picsum.photos/seed/islam5/400/400'
   }
 ];
 
 // সকালের কুরআন তিলাওয়াত সংগ্রহ
 export const MORNING_QURAN_LIST = [
-  { url: "https://server8.mp3quran.net/afs/001.mp3", title: "সুরা ফাতিহা ও তিলাওয়াত (সকাল)" },
-  { url: "https://server8.mp3quran.net/afs/055.mp3", title: "সুরা আর-রাহমান তিলাওয়াত (সকাল)" },
-  { url: "https://server8.mp3quran.net/afs/036.mp3", title: "সুরা ইয়াসিন তিলাওয়াত (সকাল)" }
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312400/Surah_Fatiha_Taqrim_rlpyte.mp3", title: "সুরা ফাতিহা" }
 ];
 
 // রাতের কুরআন তিলাওয়াত সংগ্রহ
 export const NIGHT_QURAN_LIST = [
-  { url: "https://server8.mp3quran.net/afs/067.mp3", title: "সুরা মুলক তিলাওয়াত (রাত)" },
-  { url: "https://server8.mp3quran.net/afs/073.mp3", title: "সুরা মুজাম্মিল তিলাওয়াত (রাত)" },
-  { url: "https://server8.mp3quran.net/afs/032.mp3", title: "সুরা সাজদাহ তিলাওয়াত (রাত)" }
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312323/Surah_Maryam_Sherif_Mostafa_o64dht.mp3", title: "সুরা মারইয়াম" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312279/Surah_Mulk_Abdul_Rahman_Mossad_bnyjlb.mp3", title: "সুরা মুলক" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312272/Surah_Muhammad_Zikrullah_TV_xs9enb.mp3", title: "সুরা মুহাম্মাদ" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312268/Surah_Sajdah_Abdul_Rahman_Mossad_pxemlv.mp3", title: "সুরা সাজদাহ" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312254/Surah_Yunus_Abdul_Rahman_Mossad_z67t1f.mp3", title: "সুরা ইউনুছ" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312248/Abdul_Rahman_Mossad_rnwdam.mp3", title: "আব্দুল রাহমান মোসাদ" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312200/Surah_Ar-Rahman_asbmrn.mp3", title: "সুরা আর-রাহমান" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312187/Surah_Maryam_Abdul_Rahman_Mossad_fazmhi.mp3", title: "সুরা মারইয়াম" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312160/Surah_Ankabut_Abdul_Rahman_Mossad_hps7yq.mp3", title: "সুরা আনকাবূত" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312118/Surah_An-Naba_Abdul_Rahman_Mossad_yexhpe.mp3", title: "সুরা আন-নাবা" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312111/Surah_Al-Muzzammil_Zikrullah_TV_jnoffq.mp3", title: "সুরা আল-মুযযাম্মিল" },
+  { url: "https://res.cloudinary.com/dc8vv80pc/video/upload/v1771312332/Surah_Yaseen_Zikrullah_TV_zukhvo.mp3", title: "সুরা ইয়া-সীন" }
 ];
 
 // ৫ ওয়াক্তের আজান লিংক
@@ -77,12 +140,12 @@ export const QURAN_SCHEDULE: ScheduledProgram[] = [
   {
     time: "০৬:০০ এএম", // সকালের সময় পরিবর্তন করুন এখানে
     url: "", 
-    title: "সকালের কুরআন তিলাওয়াত"
+    title: "দিনের বরকতময় শুরু"
   },
   {
     time: "১০:০০ পিএম", // রাতের সময় পরিবর্তন করুন এখানে
     url: "", 
-    title: "রাতের কুরআন তিলাওয়াত"
+    title: "রাতের প্রশান্তি"
   }
 ];
 
